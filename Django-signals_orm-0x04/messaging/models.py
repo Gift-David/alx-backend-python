e7d1a1d98ec7172b9ebf32be2ff93c373bd4be28
David Gift <giftdavid189@gmail.com>
2025-11-30 15:53:07 +0100

    implemented message history feature

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -5,8 +5,18 @@ class Message(models.Model):
     sender = models.CharField(max_length=200)
     receiver = models.CharField(max_length=200)
     content = models.TextField(null=False, blank=False)
+    edited = models.BooleanField(editable=False, default=False)
     timestamp = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
+
+class MessageHistory(models.Model):
+    sender = models.CharField(max_length=200)
+    receiver = models.CharField(max_length=200)
+    content = models.TextField(null=False, blank=False)
+    edited = models.BooleanField(editable=False, default=False)
+    timestamp = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
